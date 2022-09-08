bbd0c3163ddddb816f40f9bf492b3cca65d25fd8
Brian.Konz <brianekonz@gmail.com>
2022-09-08 12:13:32 -0300

    primera mini calcu en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calcuAngular';
+  title = 'Mini Calculadora';
+  numero1:number=0;
+  numero2:number=0;
+  resultado:number=0;
+
+  sumar():void{
+    //el void se coloca cuando no va a retornar un valor
+    this.resultado=this.numero1+this.numero2;
+  }
+
+  restar():void{
+    //el void se coloca cuando no va a retornar un valor
+    this.resultado=this.numero1-this.numero2;
+  }
+
+  multiplicar():void{
+    //el void se coloca cuando no va a retornar un valor
+    this.resultado=this.numero1*this.numero2;
+  }
+
+  dividir():void{
+    //el void se coloca cuando no va a retornar un valor
+    this.resultado=this.numero1/this.numero2;
+  }
+
+  resetear():void{
+    this.numero1=0;
+    this.numero2=0;
+    this.resultado=0
+  }
 }
